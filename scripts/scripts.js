f6e21136d77331192908d3faeb4623fc9678dce0
unknown <ronaldocreis7@gmail.com>
2021-11-01 19:16:08 -0300

    layout finished

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+var controller = new ScrollMagic.Controller();
+var scene1 = new ScrollMagic.Scene({triggerElement: "main", duration: 2000})
+.setTween("#coffee", {top: '300px', transform: 'rotate(10deg)', right: '-12vw'}) // the tween durtion can be omitted and defaults to 1
+// .addIndicators({name: "2 (duration: 2000)"}) // add indicators (requires plugin)
+.addTo(controller);
+
+var scene2 = new ScrollMagic.Scene({triggerElement: "#conhecimentos", duration: 1500})
+.setTween("#plant", {left: '-8vw'}) // the tween durtion can be omitted and defaults to 1
+// .addIndicators({name: "2 (duration: 2000)"}) // add indicators (requires plugin)
+.addTo(controller);
+
+var scene3 = new ScrollMagic.Scene({triggerElement: "#conhecimentos", duration: 2200})
+.setTween("#book", {right: '-400px'}) // the tween durtion can be omitted and defaults to 1
+// .addIndicators({name: "2 (duration: 2000)"}) // add indicators (requires plugin)
+.addTo(controller);
